572aebc6862d010ca28f179d49fa2326e9a3d12c
George-Kibe <georgekibewambui@gmail.com>
2023-01-24 17:38:20 +0300

    header slider and products

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,8 +1,20 @@
+import "./App.css"
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Home from "./pages/Home/Home";
+
+//continue from 1:16:15 npm install firebase  https://www.youtube.com/watch?v=AJmqb_Q3ojA
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello and lets do Amazon Clone</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          {/* <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/signup' element={<Signup/>} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
