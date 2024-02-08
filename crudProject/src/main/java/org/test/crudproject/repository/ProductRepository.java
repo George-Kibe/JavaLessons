abb205a12f30d9f7f6b9235e68bda33f1d3129e4
George-Kibe <georgekibewambui@gmail.com>
2024-02-08 22:19:48 +0300

    creating a crud project and some ref screenshots

---
+++ crudProject/src/main/java/org/test/crudproject/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.test.crudproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.test.crudproject.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
---
+++ crudProject/src/test/java/org/test/crudproject/CrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
