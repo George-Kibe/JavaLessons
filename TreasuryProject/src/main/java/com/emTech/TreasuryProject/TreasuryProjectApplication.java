5bc75adb4b4b2c1dcc7bf4e4457df795402852dc
George-Kibe <georgekibewambui@gmail.com>
2024-02-20 21:00:58 +0300

    mongodb crud spring boot

---
+++ TreasuryProject/src/main/java/com/emTech/TreasuryProject/TreasuryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emTech.TreasuryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasuryProjectApplication.class, args);
+	}
+
+}
---
+++ TreasuryProject/src/test/java/com/emTech/TreasuryProject/TreasuryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emTech.TreasuryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasuryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
